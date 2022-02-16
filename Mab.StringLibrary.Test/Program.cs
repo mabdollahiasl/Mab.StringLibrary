3960d3230bd088b6d6207cf29101eb9a6311df83
mabdollahiasl <abdollahiasl@hotmail.com>
2022-02-16 17:24:40 +0330

    add some thing good.

--- Mab.StringLibrary.Test/Program.cs
+++ Mab.StringLibrary.Test/Program.cs
@@ -1,15 +1,15 @@
 ﻿
 using Mab.StringLibrary;
-
+using System.Diagnostics;
 
 RandomString randomString = new();
 
 
 
-Console.WriteLine($"This is a random sentence: {randomString.GetRandomSentence(180)}");
+Console.WriteLine($"This is a random sentence: '{randomString.GetRandomSentence(180)}'");
 Console.WriteLine();
 
-Console.WriteLine($"This is a random string: {randomString.GetRandomString(35)}");
+Console.WriteLine($"This is a random string: '{randomString.GetRandomString(35)}'");
 Console.WriteLine();
 
 
@@ -19,11 +19,18 @@ StringCoder stringCoder = new StringCoder();
 long number = 54662144;
 string enCode = stringCoder.Encode(number);
 
-Console.WriteLine($"Coded value of {number} is {enCode}");
+Console.WriteLine($"Coded value of '{number}' is '{enCode}'");
 
 Console.WriteLine();
-Console.WriteLine($"Decoded value of {enCode} is {stringCoder.Decode(enCode)}");
+Console.WriteLine($"Decoded value of '{enCode}' is '{stringCoder.Decode("a3U1")}'");//214325
 
 
+string big = randomString.GetRandomString(50);
+string search = big[8..35];
 
+
+var similarity = big.CalculateSimilarity(search);
+Console.WriteLine($"Similarity between '{big}' and '{search}' is: {similarity:N2}");
+
+similarity = "this is test".CalculateSimilarity("tihs is text");
 Console.ReadKey();
--- Mab.StringLibrary/Extensions.cs
+++ Mab.StringLibrary/Extensions.cs
@@ -2,8 +2,63 @@
 
 namespace Mab.StringLibrary
 {
-    public class Extensions
+    public static class Extensions
     {
+        public static int Levenshtein(this string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.IsNullOrEmpty(target) ? 0 : target.Length;
+
+            if (string.IsNullOrEmpty(target))
+                return string.IsNullOrEmpty(source) ? 0 : source.Length;
+
+            int sourceLength = source.Length;
+            int targetLength = target.Length;
+
+            int[,] distance = new int[sourceLength + 1, targetLength + 1];
+
+            // Step 1
+            for (int i = 0; i <= sourceLength; distance[i, 0] = i++) ;
+            for (int j = 0; j <= targetLength; distance[0, j] = j++) ;
+
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= targetLength; j++)
+                {
+                    // Step 2
+                    int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    // Step 3
+                    distance[i, j] = Math.Min(
+                                        Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1),
+                                        distance[i - 1, j - 1] + cost);
+                }
+            }
+
+            return distance[sourceLength, targetLength];
+
+
+        }
+
+        /// <summary>
+        /// Compare string with target and return a number between 0 and 1 based on their similarity. 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target">String to compare</param>
+        /// <returns></returns>
+        public static double CalculateSimilarity(this string source, string target)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                return string.IsNullOrEmpty(target) ? 1 : 0;
+            }
+            if (string.IsNullOrEmpty(target))
+            {
+                return string.IsNullOrEmpty(source) ? 1 : 0;
+            }
+            double stepsToSame = Levenshtein(source, target);
+            return (1.0 - (stepsToSame / (double)Math.Max(source.Length, target.Length)));
+        }
 
     }
 }
